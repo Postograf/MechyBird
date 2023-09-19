0be2f025fe9d22531bb099d5aba9244211736e2f
Postograf <adventure686@mail.ru>
2023-09-19 23:00:51 +0300

    Увеличитель очков

---
+++ Assets/Scripts/Walls/TriggerScoreGiver.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class TriggerScoreGiver : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Bird>(out _) && Run.Current is not null)
+            Run.Current.Score++;
+    }
+}
