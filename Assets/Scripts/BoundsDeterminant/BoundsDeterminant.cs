8d0975798d22d1416921700a8f19e368befc4421
Postograf <adventure686@mail.ru>
2023-09-19 15:42:29 +0300

    Определители границ

---
+++ Assets/Scripts/BoundsDeterminant/BoundsDeterminant.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public abstract class BoundsDeterminant : MonoBehaviour
+{
+    public abstract Bounds GetBounds();
+}
---
+++ Assets/Scripts/BoundsDeterminant/CameraBounds2D.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraBounds2D : BoundsDeterminant
+{
+    [Tooltip("Main camera for default")]
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Vector2 _additionalSize;
+
+    public override Bounds GetBounds()
+    {
+        if (_camera is null)
+            _camera = Camera.main;
+
+        var extents = _camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        var size = (Vector2)extents * 2 + _additionalSize;
+        return new Bounds((Vector2)_camera.transform.position, size);
+    }
+}
