bb3b4cc88ea38717919b68da3ea9bc290bcbb6f8
Postograf <adventure686@mail.ru>
2023-09-19 15:40:11 +0300

    Конвертер Json

---
+++ Assets/Scripts/Json/JsonConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+public class JsonConverter<T>
+{
+    private string _path;
+
+    public JsonConverter(string path)
+    {
+        _path = path;
+    }
+
+    public async void Serialize(T data, bool prettyPrint)
+    {
+        using (var writer = new StreamWriter(_path))
+        {
+            await writer.WriteAsync(JsonUtility.ToJson(data, prettyPrint));
+        }
+    }
+
+    public async Task<T> Deserialize(T @default = default)
+    {
+        if (File.Exists(_path) == false)
+            return @default;
+
+        using (var reader = new StreamReader(_path))
+        {
+            var text = await reader.ReadToEndAsync();
+            return JsonUtility.FromJson<T>(text);
+        }
+    }
+}
