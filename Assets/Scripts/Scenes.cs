dc059f7d2fb94f4992c9c35ad6268f94bbb8146a
Postograf <adventure686@mail.ru>
2023-09-19 23:01:02 +0300

    Переключатель сцен

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public void Load(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Add(int index)
+    {
+        SceneManager.LoadSceneAsync(index, LoadSceneMode.Additive);
+    }
+}
