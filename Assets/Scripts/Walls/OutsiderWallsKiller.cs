edca43de8e104b23930d3365bbb643a663388cae
Postograf <adventure686@mail.ru>
2023-09-19 15:44:02 +0300

    Уничтожитель заграничных стен

---
+++ Assets/Scripts/Walls/OutsiderWallsKiller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Events;
+
+using Part = ObjectPool<Wall>.Part;
+
+public class OutsiderWallsKiller : MonoBehaviour
+{
+    [SerializeField] private BoundsDeterminant _measurer;
+
+    private Bounds _bounds;
+    private List<Part> _parts = new();
+
+    public UnityEvent<Part> WallDestroyed;
+
+    private void Awake()
+    {
+        _bounds = _measurer.GetBounds();
+    }
+
+    private void LateUpdate()
+    {
+        for (int i = 0; i < _parts.Count; i++)
+        {
+            var part = _parts[i];
+            if (_bounds.Contains(part.Value.Transform.position) == false)
+            {
+                part.Dispose();
+                _parts.RemoveAt(i);
+                WallDestroyed?.Invoke(part);
+            }
+        }
+    }
+
+    public void AddWall(Part part)
+    {
+        _parts.Add(part);
+    }
+
+    public void RemoveWall(Part part)
+    {
+        for (int i = 0; i < _parts.Count; i++)
+        {
+            var iPart = _parts[i];
+
+            if (iPart.Value == part.Value)
+            {
+                _parts.RemoveAt(i);
+                break;
+            }
+        }
+    }
+}
