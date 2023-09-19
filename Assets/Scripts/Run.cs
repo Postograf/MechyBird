06ee40b3f842845ccac67f542e8b5652dfa7d995
Postograf <adventure686@mail.ru>
2023-09-19 23:00:16 +0300

    Контроллер забега

---
+++ Assets/Scripts/Run.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Run : MonoBehaviour
+{
+    public UnityEvent Begun;
+    public UnityEvent Ended;
+    public UnityEvent<int> ScoreChanged;
+
+    public static Run Current { get; private set; }
+
+    private int _score;
+    public int Score 
+    {
+        get => _score;
+        set
+        {
+            _score = value;
+            ScoreChanged?.Invoke(_score);
+        }
+    }
+
+    public void Begin()
+    {
+        Score = 0;
+        Current = this;
+        Begun?.Invoke();
+    }
+
+    public void End()
+    {
+        Current = null;
+        Ended?.Invoke();
+    }
+}
