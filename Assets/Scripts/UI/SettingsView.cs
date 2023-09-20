905bb07da7902d51f8dd0cec0156398d1446c607
Postograf <adventure686@mail.ru>
2023-09-20 15:23:39 +0300

    Виюшка для настроек И забытые меты

--- Assets/Scripts/UI/SettingsView.cs
+++ Assets/Scripts/UI/SettingsView.cs
@@ -5,13 +5,18 @@ public class SettingsView : MonoBehaviour
 {
     [SerializeField] private Slider _volume;
     [SerializeField] private Slider _difficulty;
+	
+	private void OnEnable() 
+	{
+        _volume.value = Settings.Instance.Volume;
+        _difficulty.value = Settings.Instance.Difficulty;
+	}
 
     private void Start()
     {
-        _volume.value = Settings.Instance.Volume;
-        _difficulty.value = Settings.Instance.Difficulty;
-
         _volume.onValueChanged.AddListener((x) => Settings.Instance.Volume = x);
         _difficulty.onValueChanged.AddListener((x) => Settings.Instance.Difficulty = (int)x);
+		
+		gameObject.SetActive(false);
     }
 }
