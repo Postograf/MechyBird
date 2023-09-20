afe2cba3956b2505ef8a106e66ad2ad4ff679fd4
Postograf <adventure686@mail.ru>
2023-09-20 15:25:27 +0300

    Возможность добавлять больше 1 очка за раз

--- Assets/Scripts/Walls/TriggerScoreGiver.cs
+++ Assets/Scripts/Walls/TriggerScoreGiver.cs
@@ -3,9 +3,11 @@
 [RequireComponent(typeof(Collider2D))]
 public class TriggerScoreGiver : MonoBehaviour
 {
+	[SerializeField] private int _score;
+	
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Bird>(out _) && Run.Current is not null)
-            Run.Current.Score++;
+            Run.Current.Score += _score;
     }
 }
